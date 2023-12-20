417c934357c43490b899a57ff561e8949c7c3b54
silkfinik <wottankiazzaa@gmail.com>
2023-12-20 21:21:11 +0300

    функция с максимальной суммой чет не робит но  я заебался уже возиться с этим говном

--- main.cpp
+++ main.cpp
@@ -64,13 +64,17 @@ private:
 
 public:
     Journal()= default;
-    void read_file(std::string file_name = "input.txt");
+    void read_file(const std::string& file_name = "input.txt");
     void print_journal();
     void print_by_name();
     void print_by_avg_mark(const int& a, const int& b);
+    void print_by_subject(const std::string& str);
+    void number_of_students_by_subject();
+    void avg_mark_by_subject();
+    void print_max_sum_marks();
 };
 
-void Journal::read_file(std::string file_name){
+void Journal::read_file(const std::string& file_name){
     std::ifstream in(file_name);
     if(in.is_open()){
         std::string str;
@@ -108,7 +112,7 @@ void Journal::print_by_name(){
     std::vector <std::string> info;
     std::vector <std::vector<std::string> > vec;
     for(auto& i : students){
-        info.push_back(i.second.name);
+        info.push_back(i.second.name.substr(0, i.second.name.find(' ')));
         info.push_back(std::to_string(i.second.Number));
         info.push_back(std::to_string(i.second.avg_mark));
         vec.push_back(info);
@@ -146,6 +150,77 @@ void Journal::print_by_avg_mark(const int& a, const int& b){
     }
 }
 
+void Journal::print_by_subject(const std::string& str){
+    std::cout << str << ":" << std::endl;
+    for(auto& i : students){
+        for(auto& j : i.second.Marks){
+            if(j.subject == str){
+                std::cout << i.second.Number << " " << i.second.name << " " << j.Mark << std::endl;
+            }
+        }
+    }
+}
+
+void Journal::number_of_students_by_subject(){
+    std::map<std::string, int> subjects;
+    for(auto& i : students){
+        for(auto& j : i.second.Marks){
+            subjects[j.subject]++;
+        }
+    }
+    for(auto& i : subjects){
+        std::cout << i.first << " " << i.second << std::endl;
+    }
+}
+
+bool compare_vec_pair_by_avg_mark(const std::pair<std::string, double>& a, const std::pair<std::string, double>& b){
+    return a.second > b.second;
+}
+
+void Journal::avg_mark_by_subject(){
+    std::map<std::string, double> subjects;
+    std::map<std::string, int> avg;
+    for(auto& i : students){
+        for(auto& j : i.second.Marks){
+            avg[j.subject]++;
+        }
+    }
+    for(auto& i : students){
+        for(auto& j : i.second.Marks){
+            subjects[j.subject] += j.Mark / double(avg[j.subject]);
+        }
+    }
+    std::vector <std::pair<std::string, double> > vec;
+    for(auto& i : subjects){
+        vec.emplace_back(i.first, i.second);
+    }
+    std::sort(vec.begin(), vec.end(), compare_vec_pair_by_avg_mark);
+    for(auto& i : vec){
+        std::cout << i.first << " " << i.second << std::endl;
+    }
+}
+
+void Journal::print_max_sum_marks(){
+    std::map<std::string, int> students_marks;
+    int max = 0;
+    for(auto& i : students){
+        for(auto& j : i.second.Marks){
+            students_marks[i.second.name] += j.Mark;
+        }
+    }
+    for(auto& i : students_marks){
+        if(i.second > max){
+            max = i.second;
+        }
+    }
+    std::cout << "Students with max sum (" << max << ") of marks:" << std::endl;
+    for (auto& i : students_marks){
+        if(i.second == max){
+            std::cout << i.first << std::endl;
+        }
+    }
+}
+
 int main() {
     Journal group11;
     group11.read_file();
@@ -154,5 +229,13 @@ int main() {
     group11.print_by_name();
     std::cout << std::endl;
     group11.print_by_avg_mark(4, 10);
+    std::cout << std::endl;
+    group11.print_by_subject("Math");
+    std::cout << std::endl;
+    group11.number_of_students_by_subject();
+    std::cout << std::endl;
+    group11.avg_mark_by_subject();
+    std::cout << std::endl;
+    group11.print_max_sum_marks();
     return 0;
 }
